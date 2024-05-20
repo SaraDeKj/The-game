0a6395dbc7e456e4dc56d512fcef28fe68120357
sarad <saradekjae@gmail.com>
2024-05-20 22:59:24 +0200

    Prøver at lave baggrund til startingscreen

--- theGame-master/startingscreen.py
+++ theGame-master/startingscreen.py
@@ -1,4 +1,11 @@
 import pygame
 import os
+surface = pygame.Surface((1920, 1080))
 
 class StartingscreenClass:
+    backgroundbillede = pygame.image.load('billeder/Background.png')
+    done = False
+    while not done:
+        for event in pg.event.get():
+            surface.fill(0,0,0)
+
